a2e3d5169d2fcf9bb5d122b53c3b7f6003862d34
shohi <oshohi@gmail.com>
2019-05-17 17:20:17 +0800

    add context cancel benchmark

---
+++ pkg/benchmark/context_test.go
@@ -0,0 +1,39 @@
+package benchmark
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func BenchmarkContext(b *testing.B) {
+	b.Run("WithCancel", benchContextWithCancel)
+	b.Run("WithoutCancel", benchContextWithoutCancel)
+}
+
+func benchContextWithCancel(b *testing.B) {
+	bgCtx := context.Background()
+
+	var ch <-chan struct{}
+
+	for i := 0; i < b.N; i++ {
+		ctx, cancelFn := context.WithTimeout(bgCtx, 10*time.Millisecond)
+		ch = ctx.Done()
+		cancelFn()
+	}
+
+	_ = ch
+}
+
+func benchContextWithoutCancel(b *testing.B) {
+	bgCtx := context.Background()
+
+	var ch <-chan struct{}
+
+	for i := 0; i < b.N; i++ {
+		ctx, _ := context.WithTimeout(bgCtx, 10*time.Millisecond)
+		ch = ctx.Done()
+	}
+
+	_ = ch
+}
