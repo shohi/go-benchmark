62e710cb50e83af08f7a25d57705787714210527
shohi <oshohi@gmail.com>
2020-02-14 15:41:34 +0800

    add benchmark test for map keys

---
+++ pkg/benchmark/map_keys_test.go
@@ -0,0 +1,65 @@
+package benchmark
+
+import (
+	"testing"
+
+	// "math"
+	"reflect"
+	"strconv"
+)
+
+// refer, https://stackoverflow.com/questions/21362950/getting-a-slice-of-keys-from-a-map
+
+func newMapForBenchmark(sz int) map[string]int {
+	m := make(map[string]int, sz)
+	for i := 0; i < sz; i++ {
+		key := strconv.FormatInt(int64(i), 10)
+		m[key] = i
+	}
+
+	return m
+}
+
+func BenchmarkMapKeys(b *testing.B) {
+	m := newMapForBenchmark(100_1000)
+	cases := []struct {
+		name   string
+		keysFn func(map[string]int) []string
+	}{
+		{"traverse", keysTraverse},
+		{"reflect", keysReflect},
+	}
+
+	for _, c := range cases {
+		b.Run(c.name, func(b *testing.B) {
+			b.ResetTimer()
+			var keys []string
+			for i := 0; i < b.N; i++ {
+				keys = c.keysFn(m)
+			}
+			_ = keys
+		})
+	}
+
+}
+
+func keysTraverse(m map[string]int) []string {
+	keys := make([]string, len(m))
+	var i uint64
+	for key := range m {
+		keys[i] = key
+		i++
+	}
+
+	return keys
+}
+
+func keysReflect(m map[string]int) []string {
+	mkeys := reflect.ValueOf(m).MapKeys()
+	result := make([]string, len(mkeys))
+	for i, k := range mkeys {
+		result[i] = k.String()
+	}
+
+	return result
+}
